516e25f0b91197b80fd871ea0f73a719c0cf0845
RubyKobayashi <ruby@rubykobayashi.com>
2019-01-28 15:37:06 +0000

    Added user story and model diagram to README.md

---
+++ spec/bookmark_manager_spec.rb
@@ -0,0 +1,1 @@
+require 'bookmark_manager'
--- spec/feature/bookmark_manager_spec.rb
+++ spec/feature/bookmark_manager_spec.rb
@@ -1,8 +1,3 @@
 #As a user
 # So I can see which bookmarks I have
 # I would like to see a list of bookmarks
-
-
-
-
-![alt text](Screen Shot.png "Bookmark Manager Flow Diagram")
